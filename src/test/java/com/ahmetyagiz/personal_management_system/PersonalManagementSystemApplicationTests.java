93ee07f97d5157f196928ba6686359c16b101840
ahmetyagiz <ahmetanteryagiz@gmail.com>
2025-02-03 16:49:10 +0300

    Initial commit

---
+++ src/test/java/com/ahmetyagiz/personal_management_system/PersonalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmetyagiz.personal_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
