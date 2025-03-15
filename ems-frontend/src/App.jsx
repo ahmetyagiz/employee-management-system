5c31057a55a3e68805a0c0fcbf9452138936cc7a
ahmetyagiz <ahmetanteryagiz@gmail.com>
2025-03-15 20:34:29 +0300

    Create Frontend (React)

---
+++ ems-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import EmployeeComponent from './components/EmployeeComponent'
+
+function App() {
+
+  return (
+    <div>
+      <EmployeeComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ListEmployeeComponent() {
+    return (
+        <div>ListEmployeeComponent</div>
+    )
+}
+
+export default ListEmployeeComponent
